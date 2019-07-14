d644d55e8f56120c04ae01bf445de21ad087fec6
Kurama <uz.kurama@gmail.com>
2019-07-14 20:51:16 +0500

    1

--- widgets/FooterWidget.php
+++ widgets/FooterWidget.php
@@ -3,12 +3,16 @@
 namespace app\widgets;
 
 use yii\base\Widget;
+use app\models\Contact;
 
 
 class FooterWidget extends Widget
 {
     public function run()
     {
-        return $this->render('footer');
+        $contacts = Contact::find()->all();
+        return $this->render('footer', [
+            'contacts' => $contacts,
+        ]);
     }
 }
